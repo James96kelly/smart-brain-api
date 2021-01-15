92a12981015e029baf907bb11e24b8d14ea019c4
GlucoScan <controlbetter@gmail.com>
2021-01-15 17:09:30 +0000

    updating database

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ import {handleApiCall} from './controllers/image.js';
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-flat-15219',
+    //host : '127.0.0.1',
     user : 'postgres',
     password : 'test',
     database : 'smart-brain'
