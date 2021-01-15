cb44a63f0ad55e2b79f4781ce1689aba8257d487
GlucoScan <controlbetter@gmail.com>
2021-01-15 14:13:40 +0000

    fixed port & old ref db

--- server.js
+++ server.js
@@ -27,13 +27,17 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send("it's working");});
 app.post('/signin', handleSignin(db,bcrypt));
 app.post('/register' , (req,res) => { handleRegister(req, res, db, bcrypt);});
 app.get('/profile/:id', (req, res) => { handleProfileGet(req, res, db);});
 app.put('/image', (req, res) => { handleImage(req, res, db);});
 app.post('/imageurl', (req, res) => { handleApiCall(req, res);});
 
-app.listen(3000, ()=> {
-	console.log('app is running on port 3000');
-})
+// app.listen(3000, ()=> {
+// 	console.log('app is running on port 3000');
+// })
+
+app.listen(process.env.PORT || 3000, () => {
+console.log(`app is running on port ${process.env.PORT}`);
+});
